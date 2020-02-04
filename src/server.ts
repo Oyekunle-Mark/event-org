1cb51d400291e066e715ae688fb0bbeb9c08e353
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-04 10:04:45 +0100

    Added a todo for proper consoling

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,6 @@ const PORT = 8080;
 app.get('/', (req, res) => res.send('Hello world'));
 
 export const start = () =>
-  app.listen(PORT, console.log(`Server started at http://localhost:${PORT}`));
+  app.listen(PORT, 
+    // TODO: replace console log with winston
+    console.log(`Server started at http://localhost:${PORT}`));
