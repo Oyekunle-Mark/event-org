f51b96a85b54cae1989c6a4489d7b3343fb8101e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-04 09:08:26 +0100

    Refactored the server and start file separately

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-const express = require('express');
-const app = express();
-const PORT = 8080;
+const { start } = require('./server');
 
-app.get('/', (req, res) => res.send('Hello world'));
-
-app.listen(PORT, console.log(`Server started at http://localhost:${PORT}`));
+start();
---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+export const start = () =>
+  app.listen(PORT, console.log(`Server started at http://localhost:${PORT}`));
