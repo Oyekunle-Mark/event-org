177c4d12866edc221b16c0a78bb454fc8c23c1d1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-05 14:12:45 +0100

    Returns all events type

--- src/events/event.controller.ts
+++ src/events/event.controller.ts
@@ -2,19 +2,14 @@ import { Request, Response } from 'express';
 // import { check, sanitize, validationResult } from "express-validator";
 
 import { Events } from './event.model';
+import response from '../utils/response';
 
-export const getEvents = async (req: Request, res: Response) => {
+export const getEvents = async (req: Request, res: Response): Promise<void> => {
     try {
         const events = await Events.find().lean().exec();
 
-        res.status(200).json({
-            status: 200,
-            data: events,
-        })
+        response(res, 200, 'data', events);
     } catch (err) {
-        res.status(500).json({
-            status: 500,
-            error: `Internal server error: ${err}`
-        })
+        response(res, 500, 'error', err);
     }
 }
