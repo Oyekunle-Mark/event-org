e0c52d7f3852bd70f3059bef7d67cc9af14485ef
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-05 21:58:36 +0100

    Removed the useless validation for the delete endpoint

--- src/events/event.controller.ts
+++ src/events/event.controller.ts
@@ -63,11 +63,6 @@ export const updateEvent = async (req: Request, res: Response) => {
 
 export const deleteEvent = async (req: Request, res: Response) => {
     try {
-        await check("creator", "Event creator name must be longer than 4 characters.").isLength({ min: 4 }).run(req);
-        await check("title", "Event title must be longer than 3 characters.").isLength({ min: 3 }).run(req);
-        await check("description", "Event description must be longer than 10 characters.").isLength({ min: 10 }).run(req);
-        await check("venue", "Event venue must be longer than 5 characters.").isLength({ min: 5 }).run(req);
-        await check("isFree", "isFree property missing.").isBoolean().run(req);
         await check("id", "Request parameter must be provided").isMongoId().run(req);
 
         const errors = validationResult(req);
