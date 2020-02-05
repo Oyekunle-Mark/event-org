0596dc25f8da2a06d3f57a30a011fe766ad3ad43
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-05 14:08:14 +0100

    Handles server response

---
+++ src/utils/response.ts
@@ -0,0 +1,10 @@
+import { Response } from 'express';
+
+type flagLabel = 'data' | 'error';
+
+export default (res: Response, status: number, flag: flagLabel, data: object): Response => {
+    return res.status(status).json({
+        status,
+        [flag === 'data' ? 'data' : 'error']: flag === 'data' ? data : `Internal server error: ${data}`
+    })
+}
