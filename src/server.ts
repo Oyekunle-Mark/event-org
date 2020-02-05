54a80ca4c869d9056f2d0f99836729faa3601106
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-05 09:17:09 +0100

    Fixed return type

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.get('/', (_: Request, res: Response) => res.status(200).json({
   message: "Welcome to the event manage API"
 }));
 
-export const start = () =>
+export const start = (): void =>
   app.listen(PORT,
     // TODO: replace console log with winston
     console.log(`Server started at http://localhost:${PORT}`));
