9470f7c5cb3d0987eab631ac5c4c4b3cce207f8b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-05 16:38:12 +0100

    Sends the  data

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -5,6 +5,6 @@ type flagLabel = 'data' | 'error';
 export default (res: Response, status: number, flag: flagLabel, data: object | null): Response => {
     return res.status(status).json({
         status,
-        [flag === 'data' ? 'data' : 'error']: flag === 'data' ? data : `Internal server error: ${data}`
+        [flag === 'data' ? 'data' : 'error']: data
     })
 }
