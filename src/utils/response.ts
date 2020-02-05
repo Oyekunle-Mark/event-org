72d1846c6d2114c819c94208269a100ca788a9a8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-05 16:18:50 +0100

    Added a null option for the data type to response

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -2,7 +2,7 @@ import { Response } from 'express';
 
 type flagLabel = 'data' | 'error';
 
-export default (res: Response, status: number, flag: flagLabel, data: object): Response => {
+export default (res: Response, status: number, flag: flagLabel, data: object | null): Response => {
     return res.status(status).json({
         status,
         [flag === 'data' ? 'data' : 'error']: flag === 'data' ? data : `Internal server error: ${data}`
